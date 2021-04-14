84ee0a6324c6a25ebf4d6ab611a81fb5a61876ff
joris villet <jorisvillet@gmail.com>
2021-04-14 21:38:03 +0200

    title shadow modified

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -99,7 +99,7 @@ export default {
       font-size: 2rem;
       margin: 0 2rem 0 0;
       color: #915bfd;
-      text-shadow: 3px 3px 3px rgba(0, 0, 0, 0.2);
+      text-shadow: 4px 4px 1px rgba(0, 0, 0, 0.1);
     }
 
     &-border {
