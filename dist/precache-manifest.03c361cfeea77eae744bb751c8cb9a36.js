a25701683c1e010aca2f4b4317fe5ea30f999f79
joris villet <jorisvillet@gmail.com>
2021-04-23 15:06:28 +0200

    build

---
+++ dist/precache-manifest.03c361cfeea77eae744bb751c8cb9a36.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "704a4e9df1a0b695c395",
+    "url": "/css/app.1c102419.css"
+  },
+  {
+    "revision": "bba55c6221715929d4b70afb4ab57f33",
+    "url": "/img/iphone.bba55c62.png"
+  },
+  {
+    "revision": "2c4204ebae780aaab4be31da74a50d41",
+    "url": "/img/logo-solo-mcv.2c4204eb.png"
+  },
+  {
+    "revision": "a935ee520634b789664f138e4f8220fe",
+    "url": "/img/lune.a935ee52.jpg"
+  },
+  {
+    "revision": "4c273e8ee4587c28889efe42e4213e27",
+    "url": "/img/moi.4c273e8e.jpeg"
+  },
+  {
+    "revision": "f27ea8d5fd0bbca8a8e52c0658fe7202",
+    "url": "/img/moodboard-tea-team.f27ea8d5.jpg"
+  },
+  {
+    "revision": "b3b570628d9de1334cf091c6e3cc7966",
+    "url": "/img/moodmcvperf.b3b57062.png"
+  },
+  {
+    "revision": "00e9517d8de015e7c50b1b26f0a76370",
+    "url": "/img/postgres.00e9517d.png"
+  },
+  {
+    "revision": "c5306f4ca3e5de61e5ee803b550bc5f2",
+    "url": "/img/shop.c5306f4c.png"
+  },
+  {
+    "revision": "bd071a5559cd865bf9a25791c4f67ef7",
+    "url": "/img/sqitch.bd071a55.png"
+  },
+  {
+    "revision": "c329380351d9f0b1121c085baba899d7",
+    "url": "/img/theking.c3293803.png"
+  },
+  {
+    "revision": "98a8084bd4e0e41d829139735054c367",
+    "url": "/img/vuejs.98a8084b.png"
+  },
+  {
+    "revision": "274bf81ea78891811aaa72c46c6eb8db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "704a4e9df1a0b695c395",
+    "url": "/js/app.fbd0e1af.js"
+  },
+  {
+    "revision": "b42c7b99e70a44c13314",
+    "url": "/js/chunk-vendors.2a95b78c.js"
+  },
+  {
+    "revision": "d0db8c8e27beec178f429c1fce7bc7ea",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "0a6d5c7032739b519ff3674a94d7e0ce",
+    "url": "/media/foodtruck.0a6d5c70.mp4"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
